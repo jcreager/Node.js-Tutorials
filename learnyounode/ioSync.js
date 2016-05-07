cbbe7c28509bb86263a45851282d7d222e184786
jcreager <josephcreager@gmail.com>
2016-05-07 11:37:09 -0700

    added learnyounode solutions
    learn you node solutions for hello world, baby steps, my first io, my
    first async io

---
+++ learnyounode/ioSync.js
@@ -0,0 +1,7 @@
+var fs = require('fs')
+var file = fs.readFileSync(process.argv[2])
+console.log(file.toString().split('\n').length - 1)
+
+console.log(fs.readFileSync(process.argv[2]).toString().split('\n').length - 1)
+
+console.log(require('fs').readFileSync(process.argv[2]).toString().split('\n').length - 1)
