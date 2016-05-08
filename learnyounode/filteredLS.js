a6087f7aec15c65bba1073693277f8951a7e4e23
jcreager <josephcreager@gmail.com>
2016-05-08 15:28:11 -0700

    added lessons

--- learnyounode/filteredLS.js
+++ learnyounode/filteredLS.js
@@ -1,2 +1,19 @@
+
 var fs = require('fs')
 var path = require('path')
+
+var folder = process.argv[2]
+var ext = '.' + process.argv[3]
+
+function getFiles() {
+  return fs.readdir(folder, function (err, files) {
+    if (err) return console.error(err)
+    files.forEach(function(file) {
+      if (path.extname(file) === ext) {
+        console.log(file)
+      }
+    })
+  })
+}
+
+getFiles()
