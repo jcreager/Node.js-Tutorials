a55128ffe73aaa0a8d3094ae393f09a976d77c64
jcreager <josephcreager@gmail.com>
2016-05-09 12:42:31 -0700

    updated filteredLS.js

--- learnyounode/filteredLS.js
+++ learnyounode/filteredLS.js
@@ -1,19 +1,28 @@
-
 var fs = require('fs')
 var path = require('path')
 
-var folder = process.argv[2]
-var ext = '.' + process.argv[3]
+var dir = process.argv[2]
+var filterStr = process.argv[3]
 
-function getFiles() {
-  return fs.readdir(folder, function (err, files) {
-    if (err) return console.error(err)
-    files.forEach(function(file) {
-      if (path.extname(file) === ext) {
-        console.log(file)
-      }
+function getFiles(dir, filterStr, callback) {
+
+  fs.readdir(dir, function (err, list) {
+    if (err)
+      return callback(err)
+
+    list = list.filter(function (file) {
+      return path.extname(file) === '.' + filterStr
     })
+
+    callback(null, list)
   })
 }
 
-getFiles()
+getFiles(dir, filterStr, function (err, list) {
+  if (err)
+    return console.error('There was an error:', err)
+
+  list.forEach(function (file) {
+    console.log(file)
+  })
+})
